75ff0e01a1b7de1a435a244bdc5bf496b66f17f8
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2021-03-04 15:17:49 +0530

    added demo and changes in the methods

--- src/index.js
+++ src/index.js
@@ -1,58 +1,66 @@
 var CryptoJS = require('crypto-js');
 
+var config = {};
 function SecureStorageWeb({ name = 'app', type = 'localStorage', secretKey = 'SECRET_KEY' }) {
-  this.config = { name, type, secretKey };
+  config = { name, type, secretKey };
+  config.type = config.type === 'localStorage' ? localStorage : sessionStorage;
 }
 
 SecureStorageWeb.prototype.setItem = (name, value) => {
   if (typeof value === 'object') {
-    value = CryptoJS.AES.encrypt(JSON.stringify(data), this.config.secretKey).toString();
+    value = CryptoJS.AES.encrypt(JSON.stringify(value), config.secretKey).toString();
   } else {
-    value = CryptoJS.AES.encrypt(value, this.config.secretKey).toString();
+    value = value.toString();
+    value = CryptoJS.AES.encrypt(value, config.secretKey).toString();
   }
-
-  return this.config[type].setItem([name], value);
+  return config.type.setItem([name], value);
 };
 
 SecureStorageWeb.prototype.getItem = (name) => {
-  let value = this.config[type].getItem([name]);
+  let value = config.type.getItem([name]);
 
-  value = CryptoJS.AES.decrypt(name, this.config.secretKey);
+  value = CryptoJS.AES.decrypt(value, config.secretKey);
 
   if (typeof value === 'object') {
     value = JSON.parse(value.toString(CryptoJS.enc.Utf8));
   } else {
     value = value.toString(CryptoJS.enc.Utf8);
   }
-
   return value;
 };
 
 SecureStorageWeb.prototype.deleteItem = (name) => {
-  return this.config[type].deleteItem([name]);
+  return config.type.removeItem([name]);
 };
 
 SecureStorageWeb.prototype.getAllItems = () => {
-  const data = Object.keys(this.config[type]).reduce((obj, str) => {
-    obj[str] = this.config[type].getItem(str);
+  return Object.keys(config.type).reduce((obj, str) => {
+    obj[str] = config.type.getItem(str);
+    obj[str] = CryptoJS.AES.decrypt(obj[str], config.secretKey);
+
+    if (typeof obj[str] === 'object') {
+      obj[str] = JSON.parse(obj[str].toString(CryptoJS.enc.Utf8));
+    } else {
+      obj[str] = obj[str].toString(CryptoJS.enc.Utf8);
+    }
     return obj;
   }, {});
 };
 
 SecureStorageWeb.prototype.deleteAllItems = () => {
-  return this.config[type].clear();
+  return config.type.clear();
 };
 
 SecureStorageWeb.prototype.getAllKeys = () => {
-  Object.keys(this.config[type]).reduce((obj, str) => {
-    obj[str] = this.config[type].getItem(str);
+  const data = Object.keys(config.type).reduce((obj, str) => {
+    obj[str] = config.type.getItem(str);
     return obj;
   }, {});
-  Object.keys(data);
+  return Object.keys(data);
 };
 
 SecureStorageWeb.prototype.getLength = () => {
-  return this.config[type].length;
+  return config.type.length;
 };
 
 module.exports = SecureStorageWeb;
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
+    // export to AMD, CommonJS, or window
+    libraryTarget: 'umd',
+    // the name exported to window
     library: 'SecureStorageWeb',
     path: path.resolve(__dirname, 'dist'),
     filename: 'secure-storage-web.js',
